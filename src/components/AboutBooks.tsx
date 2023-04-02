50fc019fd3aea978c005a90cd1b550c5fc2060a3
Gosiek <gosiek.piernik@gmail.com>
2023-04-03 00:41:47 +0200

    Added component for first side

---
+++ src/components/AboutBooks.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AboutBooks = () => {
+    return <>
+    <h2>Czytanie jest ekstra!</h2>
+    </>
+}
---
+++ src/views/AboutBooksView.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {AboutBooks} from "../components/AboutBooks";
+
+
+export const AboutBooksView = () => (
+    <>
+        <AboutBooks/>
+    </>
+)
